300c90c46a6f92feb516adcad9b42f6ecdc3b60d
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-28 16:23:35 -0400

    installed b-crypt for password storage

---
+++ flask_app/models/user.py
@@ -0,0 +1,22 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash, session
+import re
+from flask_app import app
+from flask_bcrypt import Bcrypt
+from flask_app.models import exercise
+
+bcrypt = Bcrypt(app)
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+# model the class after the friend table from our database
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data ['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
