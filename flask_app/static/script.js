e10a8cb58bca1eed15210ad010f9a3a0e66254c4
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-06-28 12:51:16 -0400

    Can Display Random Pokemon Names from API

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,1 +1,17 @@
 console.log("hello!")
+
+async function getPokemonData(){
+    var response = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    var pokemonData = await response.json();
+    console.log(pokemonData)
+    randomNum=getRandomInt(20)
+    console.log(pokemonData.results[randomNum].url.sprites)
+    var pokemonName = document.getElementById("myPokemonName")
+    pokemonName.innerText=pokemonData.results[randomNum].name
+    // document.getElementById("myPokemonImg").src='{{url_for("static",filename="assets/sprites/sprites/pokemon/'randomNum'.jpeg")}}'
+    return pokemonData
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+}
