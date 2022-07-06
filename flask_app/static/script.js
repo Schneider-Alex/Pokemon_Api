aa5e55107f1a765916eb9a5e9d71c2776677c26e
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-07-06 13:13:45 -0400

    added some NES features
    NES MOUSE and pokemon becomes pokeball when caught

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -99,10 +99,13 @@ function tieFunction(selection,opponentSelection){
     document.getElementById('results').innerHTML=`<p>${window.pokemonName} has chosen ${opponentSelection.name} as well! The battle continues!</p>`
 }
 function winFunction(selection,opponentSelection){
+
     document.getElementById('selections').style="display:none"
     document.getElementById('results').innerHTML=`
     <h3>You won!</h3>
     <p>You chose ${selection.name}, which is super effective against ${window.pokemonName}'s choice, ${opponentSelection.name}! Great job, let's add them to the Pokedex!</p>`
+    document.getElementById('myPokemonImg').src=""
+    document.getElementById('caughtPokeball').innerHTML=`<i class="nes-pokeball"></i>`
     document.getElementById('new_pokemon_form').innerHTML=`<form action="/pokedex/new/pokemon" method="post">
     <Label for="name">Name your ${window.pokemonName}<input type="text" name="personal_name" minlength="2" required></Label>
     <input type="hidden" name="pokemon_name" value="${window.pokemonName}">
